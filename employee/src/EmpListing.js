980bd41538bea02c6f0141e5d7013e19066ee597
Vishal Kumar Mishra <INN921@innovdg.COM>
2024-07-28 16:40:43 +0530

    Made the border black and other changes

--- employee/src/EmpListing.js
+++ employee/src/EmpListing.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import './index.css'; // Make sure this path is correct
 
 const EmpListing = () => {
     const [empdata, empdatachange] = useState(null);
@@ -8,9 +9,11 @@ const EmpListing = () => {
     const LoadDetail = (id) => {
         navigate("/employee/detail/" + id);
     }
+    
     const LoadEdit = (id) => {
         navigate("/employee/edit/" + id);
     }
+    
     const Removefunction = (id) => {
         if (window.confirm('Do you want to remove?')) {
             fetch("http://localhost:8000/employee/" + id, {
@@ -24,9 +27,6 @@ const EmpListing = () => {
         }
     }
 
-
-
-
     useEffect(() => {
         fetch("http://localhost:8000/employee").then((res) => {
             return res.json();
@@ -36,8 +36,12 @@ const EmpListing = () => {
             console.log(err.message);
         })
     }, [])
+
     return (
         <div className="container">
+            <div className="header">
+                <h1>Employee Details</h1>
+            </div>
             <div className="card">
                 <div className="card-title">
                     <h2>Employee Listing</h2>
@@ -57,25 +61,20 @@ const EmpListing = () => {
                             </tr>
                         </thead>
                         <tbody>
-
-                            {empdata &&
-                                empdata.map(item => (
-                                    <tr key={item.id}>
-                                        <td>{item.id}</td>
-                                        <td>{item.name}</td>
-                                        <td>{item.email}</td>
-                                        <td>{item.phone}</td>
-                                        <td>
-                                            <a onClick={() => { LoadDetail(item.id) }} className="btn btn-primary">READ</a>
-                                            <a onClick={() => { LoadEdit(item.id) }} className="btn btn-success">UPDATE</a>
-                                            <a onClick={() => { Removefunction(item.id) }} className="btn btn-danger">DELETE</a>
-                                        </td>
-                                    </tr>
-                                ))
-                            }
-
+                            {empdata && empdata.map(item => (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.phone}</td>
+                                    <td>
+                                        <a onClick={() => { LoadDetail(item.id) }} className="btn btn-primary">READ</a>
+                                        <a onClick={() => { LoadEdit(item.id) }} className="btn btn-success">UPDATE</a>
+                                        <a onClick={() => { Removefunction(item.id) }} className="btn btn-danger">DELETE</a>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
-
                     </table>
                 </div>
             </div>
